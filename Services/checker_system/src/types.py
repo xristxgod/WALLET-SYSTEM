e2f3036575b7102fc3fe927bd719b301cb110d63
Mamedov Murad <mamedov_99b@mail.ru>
2022-04-21 17:29:23 +0300

    [ADD] emoji [ADD] class TelegramSmailsAndSymbols

---
+++ Services/checker_system/src/types.py
@@ -0,0 +1,6 @@
+import emoji
+
+class TelegramSmailsAndSymbols:
+    GREEN_CIRCLE = emoji.emojize(":green_circle:")
+    YELLOW_CIRCLE = emoji.emojize(":yellow_circle:")
+    RED_CIRCLE = emoji.emojize(":red_circle:")
