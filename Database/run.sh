e234d34fa68a06e072a7cfb1bfeeb4f9435467aa
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-03 14:37:10 +0300

    [ADD] bash run

---
+++ Database/run.sh
@@ -0,0 +1,2 @@
+docker-compose --file database-interface-docker-compose.yml stop
+docker-compose --file database-interface-docker-compose.yml up --build
---
+++ Database/stop.sh
@@ -0,0 +1,1 @@
+docker-compose --file database-interface-docker-compose.yml stop
