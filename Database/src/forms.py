416b41e4cf076c3e89a31c62f704e6f34b676393
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-03 14:27:41 +0300

    [ADD] new forms

--- Database/src/forms.py
+++ Database/src/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField
 from wtforms.validators import DataRequired
 
 # <<<==================================>>> Authorization forms <<<=============================================>>>
@@ -41,4 +41,24 @@ class AddTokenForm(FlaskForm):
 class UpdateTokenForm(FlaskForm):
     pass
 
-# <<<==================================>>> Token form <<<============================================================>>>
+# <<<==================================>>> Wallet form <<<===========================================================>>>
+
+class AddWalletForm(FlaskForm):
+    network = StringField(label='Network: ', validators=[DataRequired()])
+    address = StringField(label='Wallet address: ', validators=[DataRequired()])
+    private_key = StringField(label='Wallet private key: ', validators=[DataRequired()])
+    public_key = StringField(label='Wallet public key: ', validators=[DataRequired()])
+    passphrase = StringField(label='Wallet passphrase: ', validators=[DataRequired()])
+    mnemonic_phrase = StringField(label='Wallet mnemonic phrase: ', validators=[DataRequired()])
+    user_id = SelectField(label="The wallet belongs to the user: ", validators=[DataRequired()])
+
+class UpdateWalletForm(FlaskForm):
+    pass
+
+# <<<==================================>>> Wallet transaction form <<<===============================================>>>
+
+class AddWalletTransactionForm(FlaskForm):
+    pass
+
+class UpdateWalletTransactionForm(FlaskForm):
+    pass
