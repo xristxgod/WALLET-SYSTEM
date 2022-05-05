7fe8338040b8207ceb5061a997643ca45f6a7c86
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-05 15:29:27 +0300

    [ADD] main bot

--- Bots/bot_alert/config.py
+++ Bots/bot_alert/config.py
@@ -7,3 +7,4 @@ class Config(object):
     DATABASE_URL = os.getenv("DATABASE_URL")
     BOT_CHECKER_TOKEN = os.getenv("BOT_CHECKER_TOKEN")
     BOT_ALERT_TOKEN = os.getenv("BOT_ALERT_TOKEN")
+    BOT_MAIN_TOKEN = os.getenv("BOT_MAIN_TOKEN")
