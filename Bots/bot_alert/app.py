f95c0501b177506ebeb3081b17a7a5c5a1259d89
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-06 12:54:51 +0300

    [ADD] new route check sender is alive

--- Bots/bot_alert/app.py
+++ Bots/bot_alert/app.py
@@ -1,20 +1,42 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from pika import URLParameters, BlockingConnection
 
 from src.endpoints.__init__ import router
+from config import Config, logger
 
 app = FastAPI(
     title=f"BotAlert",
-    description="Service for interacting with the Tron network.",
+    description="Service for interacting with the Bot alert!",
     version="1.0.0",
     docs_url="/bot/docs",
     redoc_url="/bot/redoc",
 )
 app.include_router(router)
 
-@app.get("/", description="Find out the status of the API", response_class=JSONResponse, tags=["System"])
+@app.get("/", description="Find out the status of the API", response_class=JSONResponse, tags=["SYSTEM"])
 async def get_api_status():
     return JSONResponse(content={"message": True})
+
+@app.get("/is-sender-alive", description="Find out the status of the API", response_class=JSONResponse, tags=["SYSTEM"])
+async def get_sender_status():
+    connection: Optional[BlockingConnection] = None
+    try:
+        connection = BlockingConnection(URLParameters(url=Config.RABBITMQ_URL))
+        channel = connection.channel()
+        queue = channel.queue_declare(
+            queue=Config.RABBITMQ_QUEUE_FOR_SENDER, durable=True,
+            exclusive=False, auto_delete=False
+        )
+        return JSONResponse(content={"message": queue.method.message_count < 10})
+    except Exception as error:
+        logger.error(f"ERROR: {error}")
+        return JSONResponse(content={"message": False})
+    finally:
+        if connection is not None:
+            connection.close()
 
 if __name__ == '__main__':
     import uvicorn
--- Bots/bot_alert/config.py
+++ Bots/bot_alert/config.py
@@ -5,6 +5,9 @@ logger = logging.getLogger(__name__)
 
 class Config(object):
     DATABASE_URL = os.getenv("DATABASE_URL")
+    RABBITMQ_URL = os.getenv("RABBITMQ_URL")
+    RABBITMQ_QUEUE_FOR_SENDER = os.getenv("RABBITMQ_QUEUE_FOR_SENDER")
+
     BOT_CHECKER_TOKEN = os.getenv("BOT_CHECKER_TOKEN")
     BOT_ALERT_TOKEN = os.getenv("BOT_ALERT_TOKEN")
     BOT_MAIN_TOKEN = os.getenv("BOT_MAIN_TOKEN")
