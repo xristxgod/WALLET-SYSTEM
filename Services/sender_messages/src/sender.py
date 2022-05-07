a3a51624e34094399428899ee90f8936bd956ab1
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-07 13:44:27 +0300

    [ADD] sender in api

--- Services/sender_messages/src/sender.py
+++ Services/sender_messages/src/sender.py
@@ -1,9 +1,25 @@
+from typing import Optional, Dict
+
+import aiohttp
+
 from config import Config, logger
 
 class Sender:
     """Send a message to the bot alery api"""
     API_URL = Config.BOT_ALERT_API_URL
+    API_BALANCE = "/bot/api/user/<method>"
+
 
     @staticmethod
-    def send_if_add():
+    def _get_headers() -> Optional[Dict]:
+        """There should be an AUTH API and other things that are needed for the head."""
         pass
+
+    @staticmethod
+    def send_to_balance_method(method: str = "add", **kwargs):
+        """Send to bot alert by API | ADD/DEC balance"""
+        try:
+            async with aiohttp.ClientSession(headers=Sender._get_headers()) as session:
+                async with session.get(
+                    Sender.API_URL + ""
+                )
