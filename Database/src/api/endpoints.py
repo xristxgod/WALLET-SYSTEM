71b22a8700b08a03afa3c55d83c376fd0942c227
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-10 07:25:47 +0300

    [FIX] bugs

--- Database/src/api/endpoints.py
+++ Database/src/api/endpoints.py
@@ -1,7 +1,3 @@
 from flask import Blueprint, jsonify
 
 app = Blueprint("api", __name__, url_prefix="/api")
-
-@app.route("/health/check/isWork")
-def health_here():
-    return jsonify({"message": True})
