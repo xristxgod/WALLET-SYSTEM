00e2c17d4037ace09738dc8c7420634ba804273a
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-10 08:54:57 +0300

    [ADD] create wallet url

--- Database/src/utils/types.py
+++ Database/src/utils/types.py
@@ -1,10 +1,28 @@
+from config import Config
+
 CRYPTOAddress = str
 
-class EndpointType(object):
-    CREATE_WALLET = ("POST", "<domain>/api/<network>/create/wallet")
-    BALANCE = ("GET", "<domain>/api/<network>/balance/<address>")
+class CryptoEndpointType(object):
+    CREATE_WALLET = "<domain>/api/<network>/create/wallet"
+    BALANCE = "<domain>/api/<network>/balance/<address>"
 
-    OPTIMAL_FEE = ("GET", "<domain>/api/<network>/fee/<inputs>&<outputs>")
+    OPTIMAL_FEE = "<domain>/api/<network>/fee/<inputs>&<outputs>"
 
-    CREATE_TRANSACTION = ("POST", "<domain>/api/<network>/create/transaction")
-    SEND_TRANSACTION = ("POST", "<domain>/api/<network>/create/transaction")
+    CREATE_TRANSACTION = "<domain>/api/<network>/create/transaction"
+    SEND_TRANSACTION = "<domain>/api/<network>/create/transaction"
+
+    NETWORKS = {
+        "TRON": Config.DOMAIN_TRON_API
+    }
+
+    @staticmethod
+    def get_create_wallet_url(network: str) -> str:
+        """
+        :type network: NETWORK_TOKEN = TRON_USDT, TRON_TRX
+        :return:
+        """
+        return CryptoEndpointType.CREATE_WALLET.replace(
+            "<domain>", CryptoEndpointType.NETWORKS.get(network.split("_")[0])
+        ).replace(
+            "<network>", network.split("_")[1].lower()
+        )
