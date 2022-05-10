5019d04b35a1252f51717868ccef748840e4fac3
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-10 08:10:29 +0300

    [ADD] auth token

--- Database/config.py
+++ Database/config.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 
 class Config:
     DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:mamedov00@localhost/telegram_bot_system")
+
     DATABASE_INTERFACE_SECRET_KEY = os.getenv("DATABASE_INTERFACE_SECRET_KEY", "F441sfa1hshqsaflkasknlvonsd34124")
     DATABASE_INTERFACE_PASSWORD = os.getenv("DATABASE_INTERFACE_PASSWORD", "admin")
     DATABASE_INTERFACE_GOOGLE_AUTH_SECRET_KEY = os.getenv("DATABASE_INTERFACE_GOOGLE_AUTH_SECRET_KEY", "OBYE232HOFKHGWDS")
@@ -16,3 +17,7 @@ class Config:
     DOMAIN_TRON_API = os.getenv("DOMAIN_TRON_API")
     DOMAIN_BOT_ALERT = os.getenv("DOMAIN_BOT_ALERT")
     COIN_TO_COIN_API = os.getenv("COIN_TO_COIN_API", "https://api.coingecko.com")
+
+    AUTH_TOKEN_TRON_API = os.getenv("AUTH_TOKEN_TRON_API")
+    AUTH_BOT_ALERT = os.getenv("AUTH_BOT_ALERT")
+    AUTH_DATABASE_INTERFACE = os.getenv("AUTH_DATABASE_INTERFACE")
