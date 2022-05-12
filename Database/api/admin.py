bfcb1a792001e7034ed1ec2ff48557d15d677161
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-12 10:49:06 +0300

    [ADD] serializers.py

--- Database/api/admin.py
+++ Database/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from api.models import UserModel
 
-# Register your models here.
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "is_admin")
+    list_display_links = ("id", "username", "is_admin")
+    search_fields = ("id", "username", "is_admin")
+    list_editable = ("id", "username", "is_admin")
+    list_filter = ("id", "username", "is_admin")
+
+admin.site.reqister(UserModel, UserModelAdmin)
