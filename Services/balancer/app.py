34eb0440b5b4d3f25d0bf8b8aba257a720fbbc15
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-16 13:02:14 +0300

    [ADD] run file

---
+++ Services/balancer/app.py
@@ -0,0 +1,14 @@
+import asyncio
+import typing
+
+from src.runer import run
+
+async def main(loop: typing.Any):
+    await asyncio.gather(*[
+        run(loop=loop)
+    ])
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+    loop.close()
