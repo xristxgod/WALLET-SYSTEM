0ed4ed911fce652e827e266759738b919c6606a3
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-16 08:25:47 +0300

    [FIX]

--- Services/balancer/src/sender.py
+++ Services/balancer/src/sender.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 
 from src.external.client import Client
 from config import Config
@@ -19,6 +19,8 @@ class SenderToCryptoNode:
     @staticmethod
     def _get_correct_url(api_url: str, url: str, **kwargs):
         return api_url + SenderToCryptoNode.__get_correct_path(url=url, **kwargs)
+
+    # <<<=============================================>>> SENDER <<<=================================================>>>
 
     @staticmethod
     async def create_transaction(network: str, **kwargs) -> Optional[Dict]:
@@ -75,13 +77,29 @@ class SenderToBotAlert:
     UPDATE_TRANSACTION = "/api/create/transaction"
 
     @staticmethod
+    def _get_inputs(inputs: List[str]) -> str:
+        from_address = ""
+        for _input in inputs:
+            from_address += f"{_input} | "
+        return from_address[:-3]
+
+    @staticmethod
+    def _get_outputs(outputs: List[Dict]):
+        to_address = ""
+        for _output in outputs:
+            to_address += f"{_output.get('address')} | "
+        return to_address[:-3]
+
+    # <<<=============================================>>> SENDER <<<=================================================>>>
+
+    @staticmethod
     async def update_transaction(chat_id: int, network: str, status: int = 1, **tx_data) -> bool:
         return bool((await Client.put_request(
             url=SenderToBotAlert.API_URL + SenderToBotAlert.UPDATE_TRANSACTION,
             chatID=chat_id,
             transactionHash=tx_data.get("transaction_hash"),
-            fromAddress=tx_data.get("inputs"),
-            toAddress=tx_data.get("outputs"),
+            fromAddress=SenderToBotAlert._get_inputs(tx_data.get("inputs")),
+            toAddress=SenderToBotAlert._get_outputs(tx_data.get("outputs")),
             amount=tx_data.get("amount"),
             fee=tx_data.get("fee"),
             network=network,
