8fe75c68a147db432ea445c1a5114b204eb92df3
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-16 09:17:25 +0300

    [ADD] check balancer status, this is route

--- Bots/bot_alert/app.py
+++ Bots/bot_alert/app.py
@@ -36,6 +36,24 @@ async def get_sender_status():
         if connection is not None:
             connection.close()
 
+@app.get("/api/health/check/balancer", description="Find out the status of the API", response_class=JSONResponse, tags=["SYSTEM"])
+async def get_sender_status():
+    connection: Optional[BlockingConnection] = None
+    try:
+        connection = BlockingConnection(URLParameters(url=Config.RABBITMQ_URL))
+        channel = connection.channel()
+        queue = channel.queue_declare(
+            queue=Config.RABBITMQ_QUEUE_FOR_BALANCER, durable=True,
+            exclusive=False, auto_delete=False
+        )
+        return JSONResponse(content={"message": queue.method.message_count < 10})
+    except Exception as error:
+        logger.error(f"ERROR: {error}")
+        return JSONResponse(content={"message": False})
+    finally:
+        if connection is not None:
+            connection.close()
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run("app:app")
