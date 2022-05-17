ce9419031a3392c875a8fe05f1726a628514d780
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-17 11:53:13 +0300

    [ADD] utils and url params

--- Services/checker_system/src/checker.py
+++ Services/checker_system/src/checker.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 from datetime import datetime
 
 import asyncio
@@ -6,14 +7,27 @@ import aiohttp
 import aiofiles
 
 from src.sender import Sender
+from src.utils import Utils
 from config import ENDPOINTS_URL_PATH
 
 class Checker:
+
+    @staticmethod
+    def get_url(url: str):
+        return [u.format() for u in [url]]
+
+
+    @staticmethod
+    async def check_endpoint(**params):
+        session_params: Dict = Utils.get_headers(auth=params.get("auth"), headers=params.get("headers"))
+        request_params = {"json": params.get("data_for_check")} if params.get("data_for_check") is not None else {}
+        urls = params.get()
+
 
     @staticmethod
     async def check_all():
         async with aiofiles.open(ENDPOINTS_URL_PATH, 'r') as file:
             endpoints_url = json.loads(await file.read())
         await Sender.send_info(text='The bot for checking the system is running!')
-        await asyncio.gather(*[check_endpoint(**url_params) for url_params in endpoints_url
+        await asyncio.gather(*[Checker.check_endpoint(**url_params) for url_params in endpoints_url
         ])
---
+++ Services/checker_system/src/utils.py
@@ -0,0 +1,15 @@
+from typing import Dict
+
+class Utils:
+    @staticmethod
+    def get_headers(headers: Dict = None, auth: str = None) -> Dict:
+        if auth is None:
+            session_params = {"headers": {"Authorization": auth}}
+        else:
+            session_params = {}
+        if headers is not None:
+            if "headers" in session_params:
+                session_params["headers"].update(headers)
+            else:
+                session_params = {"headers": headers}
+        return session_params
