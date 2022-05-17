ddfeabeb194a3b16fc4fcff1104408c4982cbd2d
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-17 13:04:46 +0300

    [ADD] sender

--- Services/checker_system/src/sender.py
+++ Services/checker_system/src/sender.py
@@ -23,11 +23,26 @@ class Sender:
                 if not response.ok:
                     logger.error(f'MESSAGE NOT WAS SENT: {text}. {await response.text()}')
 
-
     @staticmethod
     async def send_info(text: str):
         await Sender._send_to_bot(text=text)
 
     @staticmethod
     async def send_news(title: str, url: str, message: str, tag: str, is_error: bool):
-        pass
+        async with lock:
+            if is_error:
+                method = "bad"
+                await storage.add_error(title)
+            else:
+                method = "good"
+                await storage.remove_error(title)
+            statuses = await storage.get_text()
+            symbol = '🟢' if len(statuses) == 0 else '🔴'
+            text = (
+                f'{symbol} (#{tag}) {title}\n'
+                f'{message}\n'
+                f'URL: {url}\n'
+                f'\n'
+                f'{statuses}'
+            )
+        await Sender._send_to_bot(text, method=method)
