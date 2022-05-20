bbf616a5f8a9a7c6c7bdd5bc91fcb91518cd5a0a
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-20 20:04:15 +0300

    [FIX]

--- Finance/Tron/demon/app.py
+++ Finance/Tron/demon/app.py
@@ -4,7 +4,4 @@ from asyncio import run
 
 if __name__ == '__main__':
     logger.error(f"DEMON IS STARTING. NETWORK: {Config.NETWORK}")
-    run(TransactionDemon().start(
-        start_block=24578584,
-        end_block=24578586
-    ))
+    run(TransactionDemon().start())
