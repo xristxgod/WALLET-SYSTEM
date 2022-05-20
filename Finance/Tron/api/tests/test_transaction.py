6f5ba30cba89012923ee2eea39e97ac1392ca83d
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-20 19:36:45 +0300

    [FIX] TESTS

--- Finance/Tron/api/tests/test_transaction.py
+++ Finance/Tron/api/tests/test_transaction.py
@@ -14,7 +14,7 @@ from config import Config, decimals
 TEST_FROM_ADDRESS = "TWCQvcJ2JkWamoXWs7rAf7PiWTYaiB8WHx"
 TEST_FROM_PRIVATE_KEY = "53054a7ebbda440df4f15b225def00dc8abc62a4a5a269a7c6023223a31d7032"
 
-TEST_TO_ADDRESS = "TRvz1r3URQq5otL7ioTbxVUfim9RVSm1hA"
+TEST_TO_ADDRESS = "THadHjK1UhZvnHaVPYNTsTDCR3mPd8XXDK"
 
 NATIVE_AMOUNT = decimals.create_decimal(0.00001)
 USDT_AMOUNT = decimals.create_decimal(0.00001)
