ca62fe8880a1e716faa6fd30e89ac47ab847c2b8
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-24 15:30:28 +0300

    [FIX]

--- DatabaseInterface/config.py
+++ DatabaseInterface/config.py
@@ -18,7 +18,7 @@ class Config(object):
     DATABASE_INTERFACE_GOOGLE_AUTH_SECRET_KEY = getenv("DATABASE_INTERFACE_GOOGLE_AUTH_SECRET_KEY", "https://medium.com/aubergine-solutions/quick-start-two-factor-authentication-in-django-admin-panel-d15ceeb62591")
     DATABASE_INTERFACE_SECRET_KEY = getenv("DATABASE_INTERFACE_SECRET_KEY", "django-insecure-yc25#g4+l$6_@q(41ct2d9zd@o!w4+yt&v8q68hv*esav^k-9n")
 
-    COIN_TO_COIN_API = getenv("COIN_TO_COIN_API", "https://api.coingecko.com")
+    COIN_TO_COIN_API = getenv("COIN_TO_COIN_API_URL", "https://api.coingecko.com")
     CRYPTO_NETWORKS_APIS = {
         "TRON": getenv("DOMAIN_TRON_API", "http://127.0.0.1:5000")
     }
